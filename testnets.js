7edfe2a592ea7d2b9151180f0a8cd8e6e5f5cc0f
yura2434 <82618826+yura2434@users.noreply.github.com>
2022-03-30 03:45:08 +0300

    Create testnets.js

---
+++ testnets.js
@@ -0,0 +1,12 @@
+  enable:
+    - bodyclose
+    - deadcode
+    - depguard
+    - dogsled
+    - dupl
+    - errcheck
+    # - funlen
+    # - gochecknoglobals
+    # - gochecknoinits
+    - goconst
+    - gocritic
